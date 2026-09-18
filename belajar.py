c0dcaf3d4260d74d4c20bd105ab5dfeea8c0981e
brian <brianprabinta@gmail.com>
2026-09-18 19:56:43 +0700

    update program python

--- belajar.py
+++ belajar.py
@@ -1,1 +1,12 @@
-print("Halo, saya Brian!")
+nama = input("Nama kamu: ")
+umur = int(input("Umur kamu: "))
+kota = input("Kota kamu: ")
+
+print("Halo,", nama + "!")
+print("umur kamu", umur, "tahun.")
+print("Saya", nama + ",", "umur", umur, "tahun,", "dan saya tinggal di", kota + ".")
+
+if umur >= 18:
+    print("kamu sudah dewasa.")
+else:
+    print("kamu masih di bawah umur.")
